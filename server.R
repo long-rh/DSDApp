b12f7905d98eda9b18cda99393e0719fe279ad6f
long-rh <55902971+long-rh@users.noreply.github.com>
2022-06-25 17:14:13 +0900

    Add files via upload

--- server.R
+++ server.R
@@ -431,7 +431,8 @@ shinyServer(function(input, output, session) {
         
         #Finding second order terms
         #second_order_candidates <- stepwise(XY_active, y="Y", selection = "forward", select="SL", Choose="AICc", sle=0.2, include = c())
-        second_order_candidates <- stepwise(Y~., data=XY_active, selection = "forward", select="AICc", sle=0.2, include = c())
+        formula = Y~.
+        second_order_candidates <- stepwise(formula=formula, data=XY_active, selection = "forward", select="AICc", sle=0.2)
         #due to the change in StepReg. see the manual.
         tmp <- second_order_candidates$process$EffectEntered
         effects_for_X2 <- tmp[-grep("intercept", tmp)]
